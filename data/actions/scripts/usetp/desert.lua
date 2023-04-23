a77fcfdeefa0e29c4ecd7b4a03b588b6757ef452
The Most Boring Person In The World Patryk Zmyslony <selppkez@gmail.com>
2023-04-23 20:50:29 +0100

    xD
    its still not working xD

--- data/actions/scripts/usetp/desert.lua
+++ data/actions/scripts/usetp/desert.lua
@@ -1,5 +1,5 @@
 function onUse(player, item, fromPosition, target, toPosition, isHotkey)
-    -- define the required vocations and positions
+    -- define the required vocations and their positions
     local required_vocations = {
         ["sorcerer"] = {1012, 1043, 8},
         ["druid"] = {1004, 1043, 8},
@@ -7,48 +7,52 @@ function onUse(player, item, fromPosition, target, toPosition, isHotkey)
         ["paladin"] = {1008, 1039, 8}
     }
     local teleport_position = {1007, 1031, 8}
-
+	
     -- define the required items and their positions
     local required_items = {
         [2674] = {1002, 1043, 8},
-        [2378] = {1008, 1048, 8},
+        [2376] = {1008, 1048, 8},
         [2455] = {1008, 1037, 8},
         [2175] = {1014, 1043, 8}
     }
-
-    -- check if all players are in the required vocations and positions
-    local players_to_teleport = {}
-    for name, pos in pairs(required_vocations) do
-        local player_vocation = player:getVocation()
-        local player_pos = player:getPosition()
-        if player_vocation:getName() == name and player_pos.x == pos[1] and player_pos.y == pos[2] and player_pos.z == pos[3] then
-            table.insert(players_to_teleport, player)
-        else
-            player:say("You do not meet the requirements to use this teleport.", TALKTYPE_MONSTER_SAY)
+    
+    -- check if all required items are present in their respective positions
+    for itemId, pos in pairs(required_items) do
+        local tile = Tile(Position(pos))
+        local itemOnTile = tile:getItemById(itemId)
+        if not itemOnTile then
+            player:sendCancelMessage("You are missing some required items.")
             return true
         end
     end
-
-    -- check if all required items are present
-    local all_items_present = true
-    for id, pos in pairs(required_items) do
-        local tile = Tile(Position(pos[1], pos[2], pos[3]))
-        local item = tile:getItemById(id)
-        if not item then
-            all_items_present = false
-            break
+    
+    -- check if players are on required vocations' positions
+    for vocation, pos in pairs(required_vocations) do
+        local tile = Tile(Position(pos))
+        local creatureOnTile = tile:getTopCreature()
+        if creatureOnTile and creatureOnTile:isPlayer() and creatureOnTile:getVocation():getName():lower() == vocation then
+            -- teleport all players to the designated position
+            creatureOnTile:teleportTo(Position(teleport_position))
+        else
+            player:sendCancelMessage("You need players of required vocations on their designated positions.")
+            return true
         end
     end
-
-    -- teleport players to the destination if all conditions are met
-    if all_items_present and #players_to_teleport == 4 then
-        for _, player in ipairs(players_to_teleport) do
-            player:teleportTo(Position(teleport_position[1], teleport_position[2], teleport_position[3]))
+    
+    -- remove all required items from the top of coal basins
+    for itemId, pos in pairs(required_items) do
+        local tile = Tile(Position(pos))
+        local itemOnTile = tile:getItemById(itemId)
+        if itemOnTile then
+            local tileAbove = Tile(Position(pos.x, pos.y, pos.z + 1))
+            local itemOnTileAbove = tileAbove:getItemById(itemId)
+            if itemOnTileAbove then
+                itemOnTileAbove:remove()
+            else
+                itemOnTile:remove()
+            end
         end
-        Game.broadcastMessage("Teleporting all players to " .. teleport_position[1] .. ", " .. teleport_position[2] .. ", " .. teleport_position[3], MESSAGE_EVENT_ADVANCE)
-    else
-        player:say("You do not meet the requirements to use this teleport.", TALKTYPE_MONSTER_SAY)
     end
-
+    
     return true
 end
