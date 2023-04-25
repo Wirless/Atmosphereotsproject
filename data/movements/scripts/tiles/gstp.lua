33ff3bcef28e69d7a8a77ef0ed5ba20d60f32d84
The Most Boring Person In The World Patryk Zmyslony <selppkez@gmail.com>
2023-04-25 11:17:27 +0100

    Update gstp.lua

--- data/movements/scripts/tiles/gstp.lua
+++ data/movements/scripts/tiles/gstp.lua
@@ -5,6 +5,20 @@ function onStepIn(creature, item, position, fromPosition)
     elseif item.uid == 17026 then
         creature:teleportTo(Position(868, 948, 7))
 		Position(868, 948, 7):sendMagicEffect(15)
+    
+	elseif item.uid == 17027 then
+        creature:teleportTo(Position(1003, 947, 7))
+		Position(1003, 947, 7):sendMagicEffect(15)
+    
+	elseif item.uid == 27027 then
+        creature:teleportTo(Position(1015, 898, 7))
+		Position(1015, 898, 7):sendMagicEffect(15)
+	elseif item.uid == 17029 then
+        creature:teleportTo(Position(955, 1127, 7))
+		Position(955, 1127, 7):sendMagicEffect(15)
+	elseif item.uid == 27029 then
+        creature:teleportTo(Position(1057, 1123, 7))
+		Position(1057, 1123, 7):sendMagicEffect(15)
     end
     return true
 end
